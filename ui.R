a4abb0d58e7e7d6a015ab43c8217135a69064328
mmonim <mam_moneim@yahoo.com>
2016-12-18 15:18:27 +0300

    Add files via upload

--- ui.R
+++ ui.R
@@ -13,16 +13,26 @@ library(shiny)
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("MTCars Data - Freuqncy of MPG in the Dataset"),
+  titlePanel("MTCars Data - Regression analysis of different dimensions of the Dataset"),
   
   # Sidebar with a slider input for value of MPG 
   sidebarLayout(
     sidebarPanel(
-       sliderInput("MPG",
-                   "Mile per Gallon:",
-                   min = 1,
-                   max = 100,
-                   value = 10)
+      
+      selectInput("variable2", "Select the FIRST dimension to carry out linear regression against:",
+                  choices = c("Miles per Gallon" = "mpg",
+                              "Horse Power" = "hp"),
+                  selected = "mpg"),
+      
+      selectInput("variable1", "Select the second dimension to for the linear regression:",
+                  choices = c("Cylinders" = "cyl",
+                              "Transmission" = "am",
+                              "Gears" = "gear"),
+                  selected = "cyl"),
+      
+      checkboxInput("Abline", "Draw AB Line", TRUE)
+   
+     
     ),
     
     # Show a plot of the generated distribution
